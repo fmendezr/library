23faf340a33754e304026a55765667a5bd016adf
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-19 19:35:08 -0400

    Make object creator function

---
+++ app.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+// Book object creater function
+const Book = (title, author, numPages, read) => {
+    this.title = title
+    this.author = author
+    this.numPages = numPages
+    this.read = read
+}
+
+// Add book to library 
+const addBookToLibrary = () => {
+}
