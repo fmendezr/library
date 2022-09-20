b294f6265a77089cbd3c62a2e4f42f840696ce1b
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-20 15:34:21 -0400

    Create functional pop-up form

--- app.js
+++ app.js
@@ -11,3 +11,25 @@ const Book = (title, author, numPages, read) => {
 // Add book to library 
 const addBookToLibrary = () => {
 }
+
+// make submit button add book 
+const addBookBtn = 
+
+
+// Make button to pop up form 
+const openModalButton = document.getElementById("button");
+const overlay = document.getElementById("overlay")
+
+openModalButton.addEventListener("click", () => {
+    const modal = document.getElementById("modal");
+    modal.classList.add("active");
+    overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", () => {
+        modal.classList.remove("active")
+        overlay.classList.remove("active")
+    }
+)
+
+
