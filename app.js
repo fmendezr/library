dac464adf7f36f28248886e4dd77a564e7647c5c
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-20 21:28:43 -0400

    Create classes of book and library. Add event listener to submit button that creates instance of book and adds it to instance of library

--- app.js
+++ app.js
@@ -1,35 +1,79 @@
-let myLibrary = [];
+// classes
 
-// Book object creater function
-const Book = (title, author, numPages, read) => {
-    this.title = title
-    this.author = author
-    this.numPages = numPages
-    this.read = read
+class Book {
+    constructor(
+        title = "unknown",
+        author = "unknown",
+        pages = "0",
+        read = false
+    ) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read;
+    }
 }
 
-// Add book to library 
-const addBookToLibrary = () => {
+class Library {
+    constructor() {
+        this.books = [];
+    }
+    addBook(newBook){
+        if(this.books.some(book => book.title == newBook.title) == false)
+            this.books.push(newBook);
+    }
+    removeBook(title){
+        this.books.filter(book => book.title != title);
+    }
+    getBook(title){
+        return this.books.find(book.title == title);
+    }
 }
 
-// make submit button add book 
-const addBookBtn = 
+// Create instance of library 
+const library = new Library;
 
+// When submit is clicked take values, create object with set values and add object to library
+const submitBook = document.getElementById("submit-book");
+submitBook.addEventListener("click", () => {
+    title = document.getElementById("title").value;
+    author = document.getElementById("author").value;
+    pages = document.getElementById("pages").value;
+    read = document.getElementById("read").checked;
+    title = new Book(title, author, pages, read);
+    library.addBook(title)
+})
 
-// Make button to pop up form 
+// Make pop up form 
 const openModalButton = document.getElementById("button");
-const overlay = document.getElementById("overlay")
+const overlay = document.getElementById("overlay");
+const modal = document.getElementById("modal");
 
 openModalButton.addEventListener("click", () => {
-    const modal = document.getElementById("modal");
     modal.classList.add("active");
     overlay.classList.add("active");
 });
 
 overlay.addEventListener("click", () => {
-        modal.classList.remove("active")
-        overlay.classList.remove("active")
+        modal.classList.remove("active");
+        overlay.classList.remove("active");
     }
 )
 
+submitBook.addEventListener("click", () => {
+        modal.classList.remove("active");
+        overlay.classList.remove("active")
+})
 
+// prevent form from reloading page
+const form = document.getElementById("modal-form")
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
+// trail btn
+const trial = document.getElementById("trial");
+
+trial.addEventListener("click", () => {
+    console.log(library)
+})
